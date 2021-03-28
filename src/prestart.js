805db6da6dce96f23a0da6d9dc7a7b3a98bf5651
ac2pic <ac2pic@gmail.com>
2021-03-28 12:59:59 -0500

    Playerskin now ignored when not Lea

--- src/prestart.js
+++ src/prestart.js
@@ -1,3 +1,34 @@
 import "./js/game/feature/quick-menu/gui/char-select-menu.js";
 import "./js/game/feature/quick-menu/gui/circle-menu/char-select.js";
 import "./js/game/feature/quick-menu/quick-menu-model/char-select.js";
+
+ig.module("game.feature.player.player-skin.char-select")
+  .requires("game.feature.player.player-skin").defines(function() {
+    sc.PlayerSkinLibrary.inject({
+        updateSkinSet: function(skinType) {
+            const player = sc.model.player;
+            if (skinType === "Appearance" && player.name !== "Lea") {
+                const currentSkin = this.currentSkins[skinType];
+                if (currentSkin) currentSkin.clearCached();
+                this.currentSkins[skinType] = null;
+                sc.Model.notifyObserver(this, sc.SKIN_EVENT.SKIN_UPDATE, skinType)
+                return;
+            }
+            this.parent(skinType);
+        }
+    });
+    ig.Game.inject({
+        init: function() {
+            this.parent();
+            sc.Model.addObserver(sc.model.player, {
+                modelChanged: function(model, messageType) {
+                    if (model === sc.model.player && messageType === sc.PLAYER_MSG.CONFIG_CHANGED) {
+                        sc.playerSkins.updateSkinSet("Appearance");
+                    }
+                }
+            });
+        }
+    });
+
+
+  });
